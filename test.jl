11efb40485dabd1e3b3dbcab18cb9641889c7ef8
Antonello Lobianco <antonello@lobianco.org>
2021-11-18 14:15:52 +0100

    first test

---
+++ test.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 89edd424-485c-11ec-09eb-1712ed5a188b
+a = 1+2
+
+# ╔═╡ 086e8c68-657c-42e4-b7e0-f2c4c51106d3
+b=a+1
+
+# ╔═╡ ca5bc530-09d8-44e8-a19e-6e9b4f9ca307
+This is not code, it is **markdown**
+
+# ╔═╡ Cell order:
+# ╠═89edd424-485c-11ec-09eb-1712ed5a188b
+# ╠═086e8c68-657c-42e4-b7e0-f2c4c51106d3
+# ╠═ca5bc530-09d8-44e8-a19e-6e9b4f9ca307
